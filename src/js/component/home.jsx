9db11fc6748b4990faea114f52af0d9b81e858d0
Zurda13 <144481470+Zurda13@users.noreply.github.com>
2023-12-03 20:12:49 +0000

    creacion de semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
-import Semaforo from "./semaforo";
+import React, { useState } from 'react'; 
+//Importamos React y useState de la biblioteca react. useState nos permite agregar estados en nuestros componentes.
+import Semaforo from "./semaforo"; 
+//Importamos el componente Semaforo.
 
 //create your first component
-const Home = () => {
+const Home = () => { //Definimos el componente Home como una función de flecha.
+	
+//Agregamos un estado llamado "selected" y una función "setSelected" para actualizar su valor. Inicialmente, selected está vacío.
+	const [selected, setSelected] = useState(""); 
 
 	return (
 
-<div className="text-center p-3 mt-3 bg-">
+<div className="container text-center p-3 bg-black">
+
 			<div className="rojo">
-				<Semaforo color={"bg-danger bg-opacity-50"}/>
+				<Semaforo color={"bg-danger"} selected={selected} setSelected={setSelected}/>
+				{/* 1."color": El color del semáforo, que es "bg-danger" "rojo" en este caso. 
+					2."selected": La variable que almacena el semáforo seleccionado. Si el semáforo rojo está seleccionado, su valor será "bg-danger". 
+					3."setSelected": La función que nos permite actualizar el valor */}
 			</div>
 			<div className="amarillo">
-				<Semaforo color={"bg-warning bg-opacity-50"}/>
+				<Semaforo color={"bg-warning"} selected={selected} setSelected={setSelected}/>
 			</div>
 			<div className="verde">
-				<Semaforo color={"bg-success bg-opacity-50"}/>	
+				<Semaforo color={"bg-success"} selected={selected} setSelected={setSelected}/>	
 			</div>
 		</div>
 	);
 };
 export default Home;
+
+// Aquí, la función "Semaforo" es un componente de función que acepta un objeto de propiedades. 
+// La propiedad "color" determina el "color" del semáforo. La propiedad "selected" nos dice cuál es el semáforo seleccionado.
+// La propiedad "setSelected" es una función que nos permite actualizar el valor de selected.
+
+// Dentro de la función "Semaforo", se define una función "on" que actualiza el valor de "selected" al color del semáforo actual 
+// cuando se hace clic en él.
+
+// El botón que representa el semáforo tiene una clase de CSS que cambia en función de si el semáforo está seleccionado o no. 
+// Si el semáforo está seleccionado, su color será el mismo que su color de fondo. Si no está seleccionado, 
+// su color de fondo será semitransparente.
+
+// Este código garantiza que, cuando un semáforo esté seleccionado, se ilumina y, cuando no esté seleccionado, 
+// regrese a su color original
--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -1,18 +1,28 @@
+// Esta línea importa dos cosas de la biblioteca React: React en sí, y la función useState. 
+// useState nos permite crear una variable en nuestro componente de React que pueda cambiar con el tiempo.
 import React, {useState} from "react";
 
-function Semaforo({color}) {
-    const [colorOn, setColorOn]=useState(color)
-    console.log(colorOn);
+// Esta línea define una función llamada Semaforo que toma tres argumentos: color, selected, y setSelected.
+// Estos argumentos nos permiten saber qué color tiene este semáforo y si está seleccionado o no, y actualizar la selección cuando el semáforo es presionado.
+function Semaforo({color, selected, setSelected}) {
 
+// Esta línea define una función llamada on. Cuando el botón del semáforo es presionado, esta función se ejecuta. 
+// Esta función simplemente llama a setSelected con el argumento color, lo que significa que este semáforo ahora está seleccionado.
     function on() {
-        colorOn === "bg-danger bg-opacity-50" ? setColorOn("bg-danger") : setColorOn("bg-danger bg-opacity-50")
-        colorOn === "bg-warning bg-opacity-50" ? setColorOn("bg-warning") : setColorOn("bg-warning bg-opacity-50")
-        colorOn === "bg-success bg-opacity-50" ? setColorOn("bg-success") : setColorOn("bg-success bg-opacity-50")
+        setSelected(color);
     }
+
     
     return (
-        <button type="button" onClick={on} className={`btn ${color} rounded-circle m-3`} style={{width: "120px", height:"120px"}}></button>
+        <button type="button" onClick={on} className={`btn ${selected === color ? color : `${color} bg-opacity-25`} rounded-circle m-3`} style={{width: "120px", height:"120px"}}></button>
+    // type="button": Indica que este botón no tiene una función predeterminada de envío de formulario.
+    // onClick={on}: Llama a la función on cuando el botón es presionado.
+    // className={...}: Define el estilo del botón. Si este semáforo está seleccionado, el botón será del color del semáforo. 
+    // Si no está seleccionado, el botón será del color del semáforo pero con una opacidad de fondo baja, lo que significa que no será tan brillante.
+    // style={{...}}: Define el tamaño del botón.
     );
 }
 
 export default Semaforo
+// Entonces, este código crea un botón de semáforo que puede ser seleccionado o no, y cambia de color cuando se presiona. 
+// Además, permite a los usuarios del semáforo cambiar fácilmente entre los diferentes modos del semáforo.
